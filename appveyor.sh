33648131fb43c2e3917f086737de3971f0687dda
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2022-06-05 12:42:52 +0900

    Fix

--- appveyor.sh
+++ appveyor.sh
@@ -2,8 +2,8 @@
 git clone https://github.com/ruby/ruby.git
 cd ruby
 git log --max-count=1
-gcc -v
 ./autogen.sh
 ./configure
-make V=1
-make V=1 test
+make
+./miniruby basictest/test.rb
+make test || true
