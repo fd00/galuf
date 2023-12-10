d115388472aa8b737c364f2d6986879f2046eef7
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2023-12-11 08:55:40 +0900

    Add parallelism & disable rdoc

--- appveyor.sh
+++ appveyor.sh
@@ -3,7 +3,7 @@ git clone https://github.com/ruby/ruby.git
 cd ruby
 git log --max-count=1
 ./autogen.sh
-./configure
-make V=1
+./configure --disable-install-rdoc
+make -j2 V=1
 ./miniruby basictest/test.rb
 make test || true
