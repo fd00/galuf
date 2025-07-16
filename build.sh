82ad3c4dab6328a939e8cf398c79394c9fce7bc4
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2025-07-16 22:33:30 +0900

    Optimize

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git clone --quiet --depth=1 https://github.com/ruby/ruby.git
-cd ruby
-git log --max-count=1
-./autogen.sh
-./configure --disable-install-rdoc
-make -j2 V=1
-./miniruby basictest/test.rb
-make test 
